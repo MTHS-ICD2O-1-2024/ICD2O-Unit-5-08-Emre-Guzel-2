9135f3c8b9de429672232a8fe4d03959c9baa016
default <yunus.emre.guzel@mths.ca>
2025-05-09 12:34:55 +0000

    formated

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: April 29 2025
+// Created on: May 8 2025
 // This file contains the JS functions for index.html
 
 "use strict"
